90ca54ece315ea3c132e88a83f1f86175aeeb5c2
Artur Shaydulin <art234562@gmail.com>
2016-09-25 13:04:17 +0300

    remade sprite

--- gulp/tasks/sprite.img.js
+++ gulp/tasks/sprite.img.js
@@ -2,25 +2,27 @@
 
 module.exports = function() {
     $.gulp.task('spriteimg', function () {
-        var spriteData = $.gulp.src('./source/images/icons/*.png').pipe($.gp.spritesmith({
+            var spriteData = $.gulp.src('./source/images/icons/*.png').pipe($.gp.spritesmith({
             imgName: 'sprite.png',
             cssName: 'sprite.css',
             algorithm: 'left-right'
         }));
-        // var imgStream = spriteData.img
-        // // DEV: We must buffer our stream into a Buffer for `imagemin`
-        //     .pipe($.gp.buffer())
-        //     .pipe($.gp.imagemin())
-        //     .pipe($.gulp.dest('./source/sprite/img'));
-        //
-        // // Pipe CSS stream through CSS optimizer and onto disk
-        // var cssStream = spriteData.css
-        //     .pipe($.gp.csso())
-        //     .pipe($.gulp.dest('./source/sprite/css'));
-        //
-        // // Return a merged stream to handle both `end` events
+
+        spriteData.img
+        // DEV: We must buffer our stream into a Buffer for `imagemin`
+            .pipe($.gp.buffer())
+            .pipe($.gp.imagemin())
+            .pipe($.gulp.dest('./source/images/sprite/img'));
+
+        // Pipe CSS stream through CSS optimizer and onto disk
+        spriteData.css
+            .pipe($.gp.csso())
+            .pipe($.gulp.dest('./source/style/css'));
+
+        // Return a merged stream to handle both `end` events
         // return $.gp.merge(imgStream, cssStream);
-        return spriteData.pipe($.gulp.dest('./source/images/sprite'))
+
+        return spriteData
     })
 };
 
